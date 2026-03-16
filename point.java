a3aeff77fe41bb13e34ea5328d411731dbf9f726
Hibabg21 <bghiba297@gmail.com>
2026-03-16 00:50:06 +0000

    update point.java

--- point.java
+++ point.java
@@ -1,12 +1,3 @@
-//Réaliser une classe Point permettant de représenter un point sur un axe. Chaque Point sera caractérisé par un nom (de type char) et une abscisse (de type double).
-//On prévoira :
-  //  • Une fonction initialiser recevant en arguments le nom et l’abscisse d’un point,
-   // • Une deuxième fonction initialiser recevant en argument un objet de type Point
-   // • une méthode affiche imprimant le nom du point et son abscisse,
-    //• une méthode translate effectuant une translation définie par la valeur de son
-//argument.
-//Écrire un petit programme utilisant cette classe pour créer un point, en afficher les
-//caractéristiques, le déplacer et en afficher à nouveau les caractéristiques.
 
 public class point {
     char nom = 'A' ;
